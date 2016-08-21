51e60f19ac7bca028f4d7148bc79802386777158
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2016-08-21 11:47:57 +0200

    Restore native platform binaries symlinks after `BigRedButton`

---
+++ scripts/symlinks.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+
+
+SYMLINKS=(addr2line ar as c++filt elfedit gprof ld ld.bfd nm objcopy objdump \
+          ranlib readelf size strings strip c++ cpp g++ gcc gcov gcov-tool)
+
+
+#
+# (Re-)create the bypass symlinks to the native platform binaries
+#
+
+for i in "${SYMLINKS[@]}"
+do
+  cp bypass.js bin/$i || exit 1
+done
+
+
+# If host platform is not based on musl, don't create un-prefixed symlinks
+readlink /lib/ld-linux.so.2 | grep musl || exit 0
+
+
+#
+# Update the bypass symbolic links to point to the cross-toolchain binaries
+#
+
+PLATFORM=
+CPU=
+
+source scripts/adjustEnvVars.sh || exit $?
+
+mkdir -p bin || exit 10
+
+for i in "${SYMLINKS[@]}"
+do
+  ln -sf $TARGET-$i bin/$i || exit 11
+done
